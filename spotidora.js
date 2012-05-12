7c92dd2c32bb9a3d56606e6f233ec0b579160bdc
Ambar Choudhury <ambarc@gmail.com>
2012-05-11 18:03:52 -0700

    More JS

--- spotidora.js
+++ spotidora.js
@@ -1,7 +1,32 @@
-p = getSpotifyApi(1);
+var sp = getSpotifyApi(1);
+var models = sp.require('sp://import/scripts/api/models');
+var player = models.player;
 
 exports.init = init;
 
 function init() {
-    console.log("init()");
+	console.log("Spotidora App Starting");
+    updatePageWithTrackDetails();
+
+    player.observe(models.EVENT.CHANGE, function (e) {
+
+        // Only update the page if the track changed
+        if (e.data.curtrack == true) {
+            updatePageWithTrackDetails();
+        }
+    });
+}
+
+function updatePageWithTrackDetails() {
+    var header = document.getElementById("header");
+
+    // This will be null if nothing is playing.
+    var playerTrackInfo = player.track;
+
+    if (playerTrackInfo == null) {
+        header.innerText = "Nothing playing!";
+    } else {
+        var track = playerTrackInfo.data;
+        header.innerHTML = track.name + " on the album " + track.album.name + " by " + track.album.artist.name + ".";
+    }
 }
