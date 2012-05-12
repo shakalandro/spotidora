6dafdc015ab3a42a1f402d8f6313bb68f73cf85e
Tyler Rigsby <tjrigs@gmail.com>
2012-05-11 19:36:48 -0700

    added music stuff

--- spotidora.js
+++ spotidora.js
@@ -25,99 +25,95 @@ $(document).ready(function() {
 	
 		onComplete : function() { }
 	});
-});
-
-exports.init = init;
 
 
-/* Playlist stuff starts */
+	/* Playlist stuff starts */
 
-var myAwesomePlaylist = new models.Playlist("My Awesome Playlist");
-var daftPunkSearch = new models.Search("Daft Punk");
-daftPunkSearch.localResults = models.LOCALSEARCHRESULTS.APPEND;
+	var myAwesomePlaylist = new models.Playlist("My Awesome Playlist");
+	var daftPunkSearch = new models.Search("Daft Punk");
+	daftPunkSearch.localResults = models.LOCALSEARCHRESULTS.APPEND;
 
-daftPunkSearch.tracks.forEach(function(track) {
-	//myAwesomePlaylist.add(track.name);
-	console.log(track.name);
-});
-
-// Currently playing. 
-//myAwesomePlaylist.add(models.player.track);
-myAwesomePlaylist.observe(models.EVENT.RENAME, function() {
-	console.log("Playlist renamed!");
-});
-
-//https://developer.spotify.com/technologies/apps/docs/beta/833e3a06d6.html
-function createPlaylist(searchQuery, playlistName) {
-	var myAwesomePlaylist = new models.Playlist(playlistName);
-	var search = new models.Search(searchQuery);
-	search.localResults = models.LOCALSEARCHRESULTS.APPEND;
-	search.observe(models.EVENT.CHANGE, function() {
-		search.tracks.forEach(function(track) {
-			console.log(track.name);
-			myAwesomePlaylist.add(track);
-		});
+	daftPunkSearch.tracks.forEach(function(track) {
+		//myAwesomePlaylist.add(track.name);
+		console.log(track.name);
 	});
-	search.appendNext();
+
 	// Currently playing. 
 	//myAwesomePlaylist.add(models.player.track);
 	myAwesomePlaylist.observe(models.EVENT.RENAME, function() {
 		console.log("Playlist renamed!");
 	});
-}
 
-function init() {
-	console.log("Spotidora App Starting");
-    updatePageWithTrackDetails();
-    pullFacebookDataTest();
+	//https://developer.spotify.com/technologies/apps/docs/beta/833e3a06d6.html
+	function createPlaylist(searchQuery, playlistName) {
+		var myAwesomePlaylist = new models.Playlist(playlistName);
+		var search = new models.Search(searchQuery);
+		search.localResults = models.LOCALSEARCHRESULTS.APPEND;
+		search.observe(models.EVENT.CHANGE, function() {
+			search.tracks.forEach(function(track) {
+				console.log(track.name);
+				myAwesomePlaylist.add(track);
+			});
+		});
+		search.appendNext();
+		// Currently playing. 
+		//myAwesomePlaylist.add(models.player.track);
+		myAwesomePlaylist.observe(models.EVENT.RENAME, function() {
+			console.log("Playlist renamed!");
+		});
+	}
 
-    player.observe(models.EVENT.CHANGE, function (e) {
+	function init() {
+		console.log("Spotidora App Starting");
+		updatePageWithTrackDetails();
+		pullFacebookDataTest();
 
-        // Only update the page if the track changed
-        if (e.data.curtrack == true) {
-            updatePageWithTrackDetails();
-        }
-    });
-}
+		player.observe(models.EVENT.CHANGE, function (e) {
 
-function pullFacebookDataTest() {
-    $.ajax({
-        url: "https://graph.facebook.com/me/friends",
-		data: {access_token: fbAccess}
-    }).done(function(data) {
-	    $('body').append(data);
-    }).fail(function(xhr, status) {
-    	$('body').append(status);
-    });
-}
+			// Only update the page if the track changed
+			if (e.data.curtrack == true) {
+				updatePageWithTrackDetails();
+			}
+		});
+	}
 
-function updatePageWithTrackDetails() {
-    var header = document.getElementById("header");
+	function pullFacebookDataTest() {
+		$.ajax({
+			url: "https://graph.facebook.com/me/friends",
+			data: {access_token: fbAccess}
+		}).done(function(data) {
+			$('body').append(data);
+		}).fail(function(xhr, status) {
+			$('body').append(status);
+		});
+	}
 
-    // This will be null if nothing is playing.
-    var playerTrackInfo = player.track;
+	function updatePageWithTrackDetails() {
+		var header = document.getElementById("header");
 
-    if (playerTrackInfo == null) {
-        header.innerText = "Nothing playing!";
-    } else {
-        var track = playerTrackInfo.data;
-        header.innerHTML = track.name + " on the album " + track.album.name + " by " + track.album.artist.name + ".";
-    }
-}
+		// This will be null if nothing is playing.
+		var playerTrackInfo = player.track;
 
-/*
-function getListens() {
-	console.log("sending ajax request");
-	$.get({
-		url: "https://graph.facebook.com/roy.miv/music.listens?access_token=" + fbAccess ,
-		success: function(response) {
-			console.log("response received: " + response);
-			var data = $("<p>");
-			data.innerHTML = response;
-			$("music").addChild(response);
+		if (playerTrackInfo == null) {
+			header.innerText = "Nothing playing!";
+		} else {
+			var track = playerTrackInfo.data;
+			header.innerHTML = track.name + " on the album " + track.album.name + " by " + track.album.artist.name + ".";
 		}
-		error: function(data) {
-			console.log("request failed: " + data);
-		}
-	});
-}*/
+	}
+	
+	function getListens() {
+		console.log("sending ajax request");
+		$.get({
+			url: "https://graph.facebook.com/roy.miv/music.listens" 
+			data: {access_token: fbAccess },
+	
+			success: function(response) {
+				$("body").append(resonse);
+			}
+			error: function(data) {
+				$("body").append(data);
+			}
+		});
+	}
+});
